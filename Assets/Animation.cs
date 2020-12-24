eb8183c32c8602958d4d12f7b33b26e61f0c851f
Ahmed-ben-Jannet1 <benjannetahmed.69@gmail.com>
2020-12-24 02:55:35 +0100

    1ere-commit

---
+++ Assets/Animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    float speed = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(20f, 200f);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); // Vector3(0, 1, 0)
+
+
+    }
+}
---
+++ Assets/MenuScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour
+{
+    public void Play()
+    { SceneManager.LoadScene("JeuAR"); }
+    public void Exit()
+    { Application.Quit(); }
+}
