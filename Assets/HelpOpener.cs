a804ec299dc1cbc45b0cab36e25c65c0f357673a
Ahmed-ben-Jannet1 <benjannetahmed.69@gmail.com>
2020-12-30 21:27:12 +0100

    MenuUpdate

---
+++ Assets/HelpOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpOpener : MonoBehaviour
+{
+    public GameObject Panel;
+    public void OpenHelp()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+  
+}
--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -6,4 +6,12 @@ public class MenuScript : MonoBehaviour
     { SceneManager.LoadScene("JeuAR"); }
     public void Exit()
     { Application.Quit(); }
+    public void Level()
+    { SceneManager.LoadScene("Levels"); }
+    public void PlayL1()
+    { SceneManager.LoadScene("JeuAR Lvl1"); }
+    public void PlayL3()
+    { SceneManager.LoadScene("JeuAR Lvl3"); }
+    public void MainMenu()
+    { SceneManager.LoadScene("MenuScene"); }
 }
